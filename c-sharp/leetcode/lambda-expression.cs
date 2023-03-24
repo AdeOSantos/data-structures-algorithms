efe41c1aa32c0c83a11b8a64597dd9186820d5bf
Adalberto Santos <adalbertoliveira@gmail.com>
2023-03-24 20:41:06 -0300

    Add in leetcode lambda-expression.cs

---
+++ c-sharp/leetcode/lambda-expression.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Project: 
+ * Purpose: 
+ * Author: Adalberto Santos.
+ * Date: 
+ * 
+ * Additional notes: 
+ *
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text; 
+using System.Runtime;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Func<string, bool> hasLength = str => str.Length > 4;
+
+        string[] countries = { "India", "United Kingdom", "US", "China", "Pakistan", "Afghanistan","Rush" };
+        IEnumerable<string> country= countries.Where(hasLength);
+        foreach (var item in country)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
+
+    }
+}
+
